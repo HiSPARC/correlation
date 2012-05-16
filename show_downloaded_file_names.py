049880c4db602fd67bfe191cd0195a60a9a55cba
Arne de Laat <adelaat@nikhef.nl>
2012-05-16 08:15:01 +0200

    Removed redundant code

--- show_downloaded_file_names.py
+++ show_downloaded_file_names.py
@@ -6,35 +6,11 @@ def show_downloaded_file_names(kind_of_data_in_table):
     print '|                                                             |'
     print '|                                        SHOWER WEATHER       |'
     print '|                                                             |'
+    blank = '|                                                             |'
     for i in kind_of_data_in_table:
-        length_string = len(i[0])+ len(str(i[1])) + len(str(i[2]))
-        if length_string == 37:
-            print '| '+ str(i) + '               |'
-        elif length_string == 38:
-            print '| '+ str(i) + '              |'
-        elif length_string == 39:
-            print '| '+ str(i) + '             |'
-        elif length_string == 40:
-            print '| '+ str(i) + '            |'
-        elif length_string == 41:
-            print '| '+ str(i) + '           |'
-        elif length_string == 42:
-            print '| '+ str(i) + '          |'
-        elif length_string == 43:
-            print '| '+ str(i) + '         |'
-        elif length_string == 44:
-            print '| '+ str(i) + '        |'
-        elif length_string == 45:
-            print '| '+ str(i) + '       |'
-        elif length_string == 46:
-            print '| '+ str(i) + '      |'
-        elif length_string == 47:
-            print '| '+ str(i) + '     |'
-        elif length_string == 48:
-            print '| '+ str(i) + '    |'
-        elif length_string == 49:
-            print '| '+ str(i) + '   |'
+        length_string = len(i[0]) + len(str(i[1])) + len(str(i[2]))
+        if length_string <= 51:
+            print '| ' + str(i) + blank[2+length_string:]
         else:
             print i[0], length_string
-
     print '|_____________________________________________________________|'
