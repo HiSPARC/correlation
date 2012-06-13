e60254122f4c4e0ec34f3475cd1c2e48a9ca1e90
Arne de Laat <adelaat@nikhef.nl>
2012-06-13 15:35:46 +0200

    Improved down_data_in_parts
    Again reduced number of lines by 50%.
    
    Made a generator function that will give the datetime ranges that split
    the interval in months (if total is more than 31 days).

--- down_data_in_parts_tot.py
+++ down_data_in_parts_tot.py
@@ -1,4 +1,6 @@
 from datetime import datetime as dt
+from datetime import timedelta as td
+from calendar import monthrange as ndays
 import re
 import os
 
@@ -8,28 +10,22 @@ from hisparc.publicdb import download_data
 
 from remove_dups_hisparcdata import remove_dups
 from remove_dups_weatherdata import remove_dups_weatherdata
-from question_is_digit import question_is_digit
-from start_end_day_of_the_month import start_end_day_of_the_month
 from check_if_weather import check_if_weather
 
 
-def diff_month(d1, d2):
-    """Calculate the number of months within a time interval"""
-    return (d2.year - d1.year)*12 + d2.month - d1.month + 1
-
-
-def down_data_in_parts(user_hisparc_station_id, user_start_date_data_interval,
+def down_data_in_parts(station_id, user_start_date_data_interval,
                        user_stop_date_data_interval):
     """ Download HiSPARC data in parts
+
+    This function will split the downloading of data into months, making
+    one file for each part. This may make working with large data sets easier
+    on slower computers.
 
     The end date is not included, it is not 'up to and including ..'
     So start=dt(2011, 6, 29), stop=dt(2011, 6, 30)
     Only downloads data from 29th June.
 
     """
-
-    # find out how many days of data the user wants to download
-
     # split user input date strings
     start_list = re.split(',', user_start_date_data_interval)
     stop_list = re.split(',', user_stop_date_data_interval)
@@ -40,157 +36,65 @@ def down_data_in_parts(user_hisparc_station_id, user_start_date_data_interval,
 
     # turn the date list into date objects
     start_date = dt(start_list_int[0], start_list_int[1], start_list_int[2])
-    end_date = dt(stop_list_int[0], stop_list_int[1], stop_list_int[2])
+    stop_date = dt(stop_list_int[0], stop_list_int[1], stop_list_int[2])
 
-    # make list of filenames with downloaded data
     downloaded_data_files = []
 
-    if start_date <= end_date and end_date <= dt.today() and start_date > dt(1998, 1, 1):
-
-        delta = end_date - start_date
-        days = delta.days
-
-        # If the pytable contains more than a month of data it gets too large to handle for my pc
-        if days <= 31:
-            start = dt(start_list_int[0], start_list_int[1], start_list_int[2])
-            stop = dt(stop_list_int[0], stop_list_int[1], stop_list_int[2])
-            filename = download_part(user_hisparc_station_id, start, stop)
-            downloaded_data_files.append(filename)
-
-        # i.e. the time interval spans MORE than 31 days
-        else:
-            check = stop_list_int[1] + stop_list_int[2]
-
-            # i.e. the timeinterval spans over two years and the stop date is not the first day of the last year (in that case the user intends to download one whole year)
-            if start_list_int[0] != stop_list_int[0] and check != 2:
-
-                # make a list of years to loop through
-                nyears = range(0, stop_list_int[0] - start_list_int[0] + 1) # e.g. [0,1]
-                years = [start_list_int[0] + x for x in nyears] # e.g. [2010,2011]
-
-                for y in years:
-                    # For the first year download first month
-                    if y == years[0]:
-                        if start_list_int[1] != 12:
-                            start = dt(y, start_list_int[1], start_list_int[2])
-                            stop = dt(y, start_list_int[1] + 1, 1)
-                            filename = download_part(user_hisparc_station_id, start, stop)
-                            downloaded_data_files.append(filename)
-                        elif start_list_int[1] == 12:
-                            start = dt(y, start_list_int[1], start_list_int[2]),
-                            stop = dt(y + 1, 1, 1)
-                            filename = download_part(user_hisparc_station_id, start, stop)
-                            downloaded_data_files.append(filename)
-                        # download the rest of the months of the first year
-                        # condition is necessary because if the december is the only month of the first year we are alredy done
-                        if start_list_int[1] != 12:
-                            months = range(start_list_int[1] + 1, 13)
-                            for m in months:
-                                first_last_day_of_the_month = start_end_day_of_the_month(start_list_int[0], m)
-                                if m != 12:
-                                    start = dt(start_list_int[0], m, first_last_day_of_the_month[0])
-                                    stop = dt(y, m + 1, 1)
-#                                    stop = dt(start_list_int[0], j+1, 1)
-                                if m == 12:
-                                    start = dt(y, m, first_last_day_of_the_month[0])
-                                    stop = dt(y + 1, 1, 1)
-#                                    start = dt(start_list_int[0], j, first_last_day_of_the_month[0])
-#                                    stop = dt(start_list_int[0]+1, 1, 1)
-                                filename = download_part(user_hisparc_station_id, start, stop)
-                                downloaded_data_files.append(filename)
-
-                    # If we are not in the first and last year
-                    if y != years[-1] and y != years[0]:
-                        months = range(1, 13)  # [1, 2, ... 12]
-                        for m in months: # Download data for all months
-                            first_last_day_of_the_month = start_end_day_of_the_month(y, m)
-                            if m != 12:
-                                start = dt(y, m, first_last_day_of_the_month[0])
-                                stop = dt(y, m + 1, 1)
-                            if m == 12:
-                                start = dt(y, m, first_last_day_of_the_month[0])
-                                stop = dt(y + 1, 1, 1)
-                            filename = download_part(user_hisparc_station_id, start, stop)
-                            downloaded_data_files.append(filename)
-
-                    # If we are in the last year
-                    if y == years[-1]:
-                        # make list of months
-                        nmonths = diff_month(date(y,1,1), end_date)
-                        months = range(1, nmonths+1)
-
-                        for m in months:
-                            first_last_day_of_the_month = start_end_day_of_the_month(y, m)
-                            start = dt(y, m, first_last_day_of_the_month[0])
-                            if m != months[-1]: # if we are not in the last month of the last year
-                                stop = dt(y, m + 1, 1)
-                            # if we are in the last month of the last year
-                            if m == months[-1]:
-                                stop = dt(y, stop_list_int[1], stop_list_int[2])
-                                # stop = dt(i, j, stop_list_int[2])
-                            filename = download_part(user_hisparc_station_id, start, stop)
-                            downloaded_data_files.append(filename)
-
-            # i.e. the time interval is shorter than one year or equal to one year
-            else:
-                nmonths = diff_month(start_date, end_date)
-                if stop_list_int[2] == 1:
-                    months = range(start_list_int[1], start_list_int[1] + nmonths - 1)
-                else:
-                    months = range(start_list_int[1], start_list_int[1] + nmonths)
-
-                for m in months:
-                    first_last_day_of_the_month = start_end_day_of_the_month(start_list_int[0], m)
-                    # finish first month of data
-                    if m == months[0]:
-                        start = dt(start_list_int[0], m, start_list_int[2])
-                        stop = dt(stop_list_int[0], m + 1, 1)
-                    # if we are NOT downloading the first month of data and also NOT the last month of data
-                    if m != months[0] and m != months[-1]:
-                        start = dt(start_list_int[0], m, first_last_day_of_the_month[0])
-                        stop = dt(stop_list_int[0], m + 1, 1)
-                    # if we are downloading the last month of data
-                    if m == months[-1]:
-                        if stop_list_int[2] == 1:
-                            start = dt(start_list_int[0], m, first_last_day_of_the_month[0])
-                            stop = dt(stop_list_int[0], m + 1, 1)
-                            # stop = dt(stop_list_int[0], stop_list_int[1], stop_list_int[2])
-                        else:
-                            start = dt(start_list_int[0], m, first_last_day_of_the_month[0])
-                            stop = dt(stop_list_int[0], stop_list_int[1], stop_list_int[2])
-                    filename = download_part(user_hisparc_station_id, start, stop)
-                    downloaded_data_files.append(filename)
-
+    if start_date >= end_date:
+        print 'Error: Start date is after stop date!'
+    elif end_date >= dt.today():
+        print 'Error: End date is after today!'
+    elif start_date < dt(2004, 1, 1):
+        print 'Error: Start date is before the start of the HiSPARC project!'
     else:
-        print 'Error in date input'
-        print 'Either your stop date is before your start date or you entered a date from before the start of HiSPARC'
-
-    print 4 * "\a"
+        for start, stop in monthrange(start_date, stop_date):
+            filename = download_part(station_id, start, stop)
+            downloaded_data_files.append(filename)
+    print 2 * "\a"
     return downloaded_data_files
 
 
-def download_part(user_hisparc_station_id, start, stop):
+def monthrange(start, stop):
+    """ A generator function that yields datetime objects in ranges of months
 
+    If start and stop are more than 31 days apart the interval is split into
+    months. The first will be from start until the end of its month, then
+    each month upto the month of stop, and finally from the start of that
+    month until stop.
+
+    """
+    a_day = td(days = 1)
+    if (stop - start).days < 31:
+        yield start, stop
+        return
+    else:
+        cur = start + td(days = ndays(start.year, start.month)[1] - start.day + 1)
+        yield start, cur
+        while cur + td(days = ndays(cur.year, cur.month)[1]) < stop:
+            yield cur, cur + td(days = ndays(cur.year, cur.month)[1])
+            cur += td(days = ndays(cur.year, cur.month)[1])
+        yield cur, stop
+        return
+            
+
+def download_part(station_id, start, stop):
     """ Download HiSPARC data from start till stop
 
     """
-    hisp_station = int(user_hisparc_station_id)
-    tree = '/s' + user_hisparc_station_id
-    path = 'data.root.s' + user_hisparc_station_id
+    hisp_station = int(station_id)
+    tree = '/s' + station_id
+    path = 'data.root.s' + station_id
     filename = 'data_s%d_%s_%s.h5' % (hisp_station,
                                       start.strftime('%Y,%m,%d'),
                                       stop.strftime('%Y,%m,%d'))
-
     with tables.openFile(filename, 'w') as data:
         download_data(data, tree, hisp_station, start, stop)
         remove_dups(data, eval(path))
         weather_data_in_file = check_if_weather(data, filename)
         if weather_data_in_file:
             remove_dups_weatherdata(data, eval(path))
-
     print ''
     print "'" + filename + "' has downloaded."
     print 'You can find it at the location: ' + os.getcwd()
     print ''
-
     return filename
