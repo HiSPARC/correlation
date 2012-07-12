c732942f2c6078ae975e8bd35076442a7e73e3cc
Arne de Laat <adelaat@nikhef.nl>
2012-07-12 23:49:14 +0200

    Improve speed of finding operational stations
    Instead of download all data from yesterday
    the function now tries to look at the data.hisparc.nl
    to determine if a station had data for yesterday
    It now takes only 3 minutes to determine if there is
    weather and/or shower data available

--- search_operational_stations.py
+++ search_operational_stations.py
@@ -1,145 +1,134 @@
-from datetime import datetime, date, timedelta
 import re
 import os
-import tables
-from pylab import *
-from hisparc.publicdb import download_data
 import time
+from datetime import datetime as dt, timedelta
+import urllib2
+import httplib
+
 from query_yes_no import query_yes_no
-
-def read_list_from_file():
-
-    print ''
-    print 'The last check for operational stations was at: ' + time.ctime(os.path.getctime('operational_hisparc_and_weather_station_IDs.txt'))
-    print ''
-
-
-
-    da = open('operational_hisparc_and_weather_station_IDs.txt','r')
-
-
-    list = da.readlines() # returns a list. Every item from the list is a string containing a line from the file.
-
-    a = list[0].replace('\n', '') # to remove the newline sign from the first line of the file
-
-    b = a.replace('operational_hisparc_station_IDs = ', '') # remove the text from the first line
-
-    c = list[1].replace('operational_weather_station_IDs = ', '') # remove the text from the second line
-
-    operational_hisparc_station_IDs = eval(b) # creates a list with hisparc station IDs
-
-    operational_weather_station_IDs = eval(c) # creates a list with weather station IDs
-
-    da.close() # close the file
-
-    print '- Currently we have the following HiSPARC stations operational:'
-    print operational_hisparc_station_IDs
-    print ''
-    print '- Currently we have the following WEATHER stations operational:'
-    print operational_weather_station_IDs
-    print ''
 
 
 def search_operational_stations():
+    """Find active shower and weather stations"""
 
-    if os.path.isfile('operational_hisparc_and_weather_station_IDs.txt'):
+    if os.path.isfile('active_station_IDs.txt'):
         read_list_from_file()
     else:
+        print ''
         print 'You do not have a list with operational stations in your current working directory.'
 
-    update = query_yes_no('Do you want to update/create the list with operational stations? (this can take about 20 minutes) ')
+    update = query_yes_no('Do you want to update/create the list with operational stations?')
     print ''
 
     if update == True:
-
-        stationID_list = [20001, 20002, 20003, 401, 402, 7201, 3301, 3302, 3303, 2, 3, 5, 6, 7, 9, 10, 13, 21, 22, 13001, 11001, 7301, 8001, 8002, 8003, 8004, 8005, 8006, 8007, 8099, 7001, 7002, 7003, 4001, 4002, 4003, 4004, 4099, 7401, 201, 202, 40001, 7101, 601, 701, 301, 302, 303, 304, 3001, 3002, 3003, 3099, 3201, 3202, 3203, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 8201, 1, 19, 96, 97, 98, 99, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511, 512, 513, 10001, 10002, 3401, 8101, 8102, 8103, 8104, 8105, 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1099, 2101, 2102, 2103, 30001, 8301, 8302, 8303, 101, 102, 103, 3101, 3102, 3103, 3104]
-        #stationID_list = [20001, 501, 502]
-
-        operational_hisparc_station_IDs = []
-        operational_weather_station_IDs = []
-
-        yesterday = str(date.today() - timedelta(days=1))
-        yest_without_zero = yesterday.replace('-0',',')
-        yest_without_hyphen = yest_without_zero.replace('-',',')
-        start_list = re.split(',',yest_without_hyphen)
-        start_list_int = [int(x) for x in start_list] # turn the list of strings into integers
-
-        today = str(date.today())
-        today_without_zero = today.replace('-0',',')
-        today_without_hyphen = today_without_zero.replace('-',',')
-        stop_list = re.split(',',today_without_hyphen)
-        stop_list_int = [int(x) for x in stop_list]
-
-        for i in stationID_list:
-
-            filename = str(i)+'_'+ yesterday + '.h5'
-            tree = '/s' + str(i)
-
-            data = tables.openFile(filename, 'w')
-            download_data(data, tree, i,
-                      start=datetime.datetime(start_list_int[0], start_list_int[1], start_list_int[2]),
-                      end=datetime.datetime(stop_list_int[0], stop_list_int[1], stop_list_int[2]))
-
-            data.close()
-
-            data = tables.openFile(filename, 'r')
-            table = data.root
-            children = table._v_nchildren
-
-            if not children:
-                data.close()
-                pass
-            else:
-
-                folder = 's' + str(i)
-                group = 'data.root.' + folder
-
-                if 'events' in eval(group):
-                    operational_hisparc_station_IDs.append(i)
-
-                if 'weather' in eval(group):
-                    operational_weather_station_IDs.append(i)
-
-                data.close()
-            os.remove(filename)
-
-        sorted_hisp_ID = sort(operational_hisparc_station_IDs)
-        sorted_weath_ID = sort(operational_weather_station_IDs)
-
-        file = open('operational_hisparc_and_weather_station_IDs.txt','w')
-        file.write('operational_hisparc_station_IDs = [')
-
-        for i in range(len(sorted_hisp_ID)):
-            if i != range(len(sorted_hisp_ID))[-1]:
-                file.write("%s, " % sorted_hisp_ID[i])
-            elif i == range(len(sorted_hisp_ID))[-1]:
-                file.write("%s" % sorted_hisp_ID[i])
-            else:
-                print 'problem'
-
-        file.write(']')
-
-        file.write("%s\n" % (''))
-        file.write('operational_weather_station_IDs = [')
-
-        for i in range(len(sorted_weath_ID)):
-            if i != range(len(sorted_weath_ID))[-1]:
-                file.write("%s, " % sorted_weath_ID[i])
-            elif i == range(len(sorted_hisp_ID))[-1]:
-                file.write("%s" % sorted_weath_IDID[i])
-            else:
-                print 'problem'
-
-        file.write(']')
-
-        file.close()
-
+        active_shower, active_weather = get_active_stations()
+        with open('active_station_IDs.txt', 'w') as file:
+            file.write('active_hisparc = [')
+            file.write(', '.join(str(id) for id in active_shower))
+            file.write(']\nactive_weather = [')
+            file.write(', '.join(str(id) for id in active_weather))
+            file.write(']')
         read_list_from_file()
 
 
+def read_list_from_file():
+    """Read the file with the list of active stations"""
+
+    print ''
+    print ('The last check for operational stations was at: ' +
+           time.ctime(os.path.getctime('active_station_IDs.txt')))
+    print ''
+
+    with open('active_station_IDs.txt', 'r') as file:
+
+        list = file.readlines() # returns a list. Every item from the list is a string containing a line from the file.
+
+        a = list[0].replace('\n', '') # to remove the newline sign from the first line of the file
+        b = a.replace('active_hisparc = ', '') # remove the text from the first line
+        c = list[1].replace('active_weather = ', '') # remove the text from the second line
+
+        active_shower = eval(b) # list of active hisparc station IDs
+        active_weather = eval(c) # list of active weather station IDs
+
+    print '- Currently we have the following HiSPARC stations operational:'
+    print active_shower
+    print ''
+    print '- Currently we have the following WEATHER stations operational:'
+    print active_weather
+    print ''
 
 
+def get_active_stations():
+    """Check which of the given stations had any shower or weather data yesterday"""
+
+    station_ids = get_station_ids()
+    active_stations = active_station(station_ids)
+    active_shower = active_shower_station(active_stations)
+    active_weather = active_weather_station(active_stations)
+
+    return active_shower, active_weather
 
 
+def active_shower_station(station_ids):
+    """Check which of the given stations had any shower data yesterday"""
+
+    active_ids = []
+    for id in station_ids:
+        url = "/django/show/source/eventtime/%s/%s" % (id, yesterday_url())
+        if url_exists(url):
+            active_ids.append(id)
+    return active_ids
 
 
+def active_weather_station(station_ids):
+    """Check which of the given stations had any weather data yesterday"""
+
+    active_ids = []
+    for id in station_ids:
+        url = "/django/show/source/barometer/%s/%s" % (id, yesterday_url())
+        if url_exists(url):
+            active_ids.append(id)
+    return active_ids
+
+
+def active_station(station_ids):
+    """Check which of the given stations had any data yesterday"""
+
+    active_ids = []
+    for id in station_ids:
+        url = "/django/show/stations/%s/%s" % (id, yesterday_url())
+        if url_exists(url):
+            active_ids.append(id)
+    return active_ids
+
+
+def get_station_ids():
+    """Get list of all station ids on the Station List page"""
+
+    url = "http://data.hisparc.nl/django/show/stations/"
+    page = urllib2.urlopen(url).read()
+    id_strings = re.findall('(?<=stationNumber\"\>)[0-9]+', page)
+    station_ids = [int(id) for id in id_strings]
+    station_ids.sort()
+    return station_ids
+
+
+def url_exists(url, base="data.hisparc.nl"):
+    """Check if the url exists (returns 200)"""
+
+    conn = httplib.HTTPConnection(base)
+    conn.request("HEAD", url)
+    res = conn.getresponse()
+    return res.status in (200, )
+
+
+def yesterday_url():
+    """Make the part of the url for the date (yesterday)"""
+
+    yesterday = dt.now() - timedelta(days = 1)
+    url = "%s/%s/%s/" % (yesterday.year, yesterday.month, yesterday.day)
+    return url
+
+
+if __name__ == '__main__':
+    search_operational_stations()
