5569807e389209b5682d3833248b950cef8bcd69
Arne de Laat <adelaat@nikhef.nl>
2012-07-16 11:38:09 +0200

    Convert for, if and append to list comprehension

--- get_number_of_plates.py
+++ get_number_of_plates.py
@@ -1,11 +1,6 @@
 def get_number_of_plates(variable):
 
-    plate_list = []
-    for val in variable:
-        if val == -1:
-            pass
-        else:
-            plate_list.append(val)
-
+    plate_list = [val for val in variable if val != -1]
     number_of_plates = len(plate_list)
+
     return number_of_plates
