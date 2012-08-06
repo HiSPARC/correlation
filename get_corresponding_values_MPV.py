a1e9e01e0e2fd45258ef8632d26146a0e7ab3b89
Arne de Laat <adelaat@nikhef.nl>
2012-08-06 18:27:15 +0200

    Change openFile to use with ... as ...

--- get_corresponding_values_MPV.py
+++ get_corresponding_values_MPV.py
@@ -10,22 +10,20 @@ def get_corresponding_values_MPV(plot_variable2, times):
 
     for i in range(len(plot_variable2)):
         # open datafile
-        data = tables.openFile(plot_variable2[i][1], 'r')
+        with tables.openFile(plot_variable2[i][1], 'r') as data:
+            # get variable values from data file
+            var_string = "data.root.s%s.%s.col('%s')" % (plot_variable2[i][2], plot_variable2[i][3], plot_variable2[i][0])
+            var = eval(var_string)
 
-        # get variable values from data file
-        var_string = "data.root.s%s.%s.col('%s')" % (plot_variable2[i][2], plot_variable2[i][3], plot_variable2[i][0])
-        var = eval(var_string)
-
-        # get timestamp values corresponding to the variable values from datafile
-        ts_string = "data.root.s%s.%s.col('timestamp')" % (plot_variable2[i][2], plot_variable2[i][3])
-        ts = eval(ts_string)
-
-        data.close()
+            # get timestamp values corresponding to the variable values from datafile
+            ts_string = "data.root.s%s.%s.col('timestamp')" % (plot_variable2[i][2], plot_variable2[i][3])
+            ts = eval(ts_string)
 
         data_sorted.extend(sorted(zip(ts, var))) # one list with timestamps and variable values
 
     if plot_variable2[0][0] in low_limit:
         bad_data2 = []
+
         for t2, v2 in data_sorted:
             if v2 >= low_limit[plot_variable2[0][0]] and v2 <= high_limit[plot_variable2[0][0]]:
                 var_list_without_bad_data2.append((t2, v2))
